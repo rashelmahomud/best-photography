6a68e898648d1957b30a19767fa1d9bdaa677dac
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-04-18 13:29:55 +0600

    added abount editing

--- src/Componests/About/About.js
+++ src/Componests/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <div>
                 <h1>-DREAM FROM <br />
                     WEB DEVELOPMENT-</h1>
-                    <p>Hello My Name is Rashel Mahomud. I have good dream for programing related work do. I can my life setup with web Development and computer sience related work todo its my big dreams. My favorite work programing and learn its best work i mena. I can my life a future all time this work with in my everythings.</p>
+                    <p>Hello My Name is Rashel Mahomud. I have good dream for programing related work do. I can my life setup with web Development and computer sience related work todo its my big dreams. My favorite work programing and learn its best work i mena. I can my life a future all time this work with in my everythings. Web Devlopment is a very currntly suitable and jentel persona and dedicated person and heard work todo person work.</p>
             </div>
         </div>
     );
